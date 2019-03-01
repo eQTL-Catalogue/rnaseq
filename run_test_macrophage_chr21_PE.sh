6f642d3f8b1620c20588b031c7b00b3c9bbfc4f9
kerimoff <kerimov.nurlan@gmail.com>
2019-03-01 12:53:29 +0200

    added testData running script

---
+++ run_test_macrophage_chr21_PE.sh
@@ -0,0 +1,15 @@
+nextflow run main.nf\
+ -profile tartu_hpc\
+ --readPathsFile 'data/readPathsFile_macrophages_PE.tsv'\
+ --reverse_stranded\
+ --hisat2_index /gpfs/hpc/home/a72094/annotations/GRCh38/hisat2_index_v90/Homo_sapiens.GRCh38.dna.primary_assembly\
+ --aligner 'hisat2'\
+ --skip_qc\
+ --skip_skip_multiqc\
+ --saveReference\
+ --saveTrimmed\
+ --saveAlignedIntermediates\
+ --gtf annotation/gtf/gencode.v29.annotation.gtf\
+ --fasta annotation/GRCh38/Homo_sapiens.GRCh38.dna.primary_assembly.fa\
+ --txrevise_gffs 'annotation/txrevise_gff/*.gff3'\
+ # -resume
