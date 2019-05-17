05d1274b36f5a2d5552b37e2d1c48162263b4b12
Kaur Alasoo <kaur.alasoo@gmail.com>
2019-05-17 11:03:33 +0300

    Updated paths to annotations

--- run_test_macrophage_chr21_PE.sh
+++ run_test_macrophage_chr21_PE.sh
@@ -1,8 +1,8 @@
 nextflow run main.nf\
  -profile tartu_hpc\
- --readPathsFile 'data/readPathsFile_macrophages_PE.tsv'\
+ --readPathsFile '/gpfs/hpc/home/a72094/datasets/open_access/testdata/readPathsFile_macrophages_PE.tsv'\
  --reverse_stranded\
- --hisat2_index /gpfs/hpc/home/a72094/annotations/GRCh38/hisat2_index_v90/Homo_sapiens.GRCh38.dna.primary_assembly\
+ --hisat2_index /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/hisat2_index_v96/Homo_sapiens.GRCh38.dna.primary_assembly\
  --aligner 'hisat2'\
  --skip_qc\
  --skip_skip_multiqc\
@@ -13,6 +13,6 @@ nextflow run main.nf\
  --run_exon_quant\
  --saveTrimmed\
  --saveAlignedIntermediates\
- --gtf annotation/gtf/gencode.v29.annotation.gtf\
- --fasta annotation/GRCh38/Homo_sapiens.GRCh38.dna.primary_assembly.fa\
- --txrevise_gffs 'annotation/txrevise_gff/*.gff3'
+ --gtf /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/gencode.v30.annotation.no_chr.gtf\
+ --fasta /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/Homo_sapiens.GRCh38.dna.primary_assembly.fa\
+ --txrevise_gffs '/gpfs/hpc/home/a72094/annotations/GRCh38/txrevise_gff/*.gff3'
