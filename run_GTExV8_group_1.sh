c2cdc90b8b8c21bd6a6ef019bf1f89286d3640be
kerimoff <kerimov.nurlan@gmail.com>
2021-02-09 08:38:27 +0200

    added GTExV8 run scripts

---
+++ run_GTExV8_group_1.sh
@@ -0,0 +1,13 @@
+nextflow run main.nf\
+ -profile tartu_hpc,eqtl_catalogue\
+ --readPathsFile /gpfs/space/projects/GTEx/GTExV8_group_readpaths/group_1_readpaths.tsv\
+ --unstranded\
+ --aligner 'hisat2'\
+ --run_tx_exp_quant\
+ --run_txrevise\
+ --run_exon_quant\
+ --saveReference\
+ --run_mbv\
+ --mbv_vcf /gpfs/space/projects/GTEx/genotypes/processed/GTEx.MAF001.vcf.gz\
+ --outdir /gpfs/space/projects/GTEx/rnaseq/group_1\
+ -resume
